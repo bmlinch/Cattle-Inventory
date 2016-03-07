8f638276813fca180f389eff35fd6160a2c23900
Accidental82 <Jason.sullenger@gmail.com>
2016-03-07 15:44:14 -0700

    can db

--- public/apps/components/addTank/addTank.js
+++ public/apps/components/addTank/addTank.js
@@ -1,9 +1,20 @@
-app.controller('AddTank', function($scope){
+app.controller('AddTank', function($rootScope, $scope, Models){
     // pushes canisters to database
-    this.addTank = function(){
-        debugger
-        for(var i = 0; i <$scope.canisternum.length; i++){
-           tank.canisters.push(canisters[i]) 
-        }
+    Models.Tank.findAll()
+    Models.Tank.bindAll({where:{userId: $rootScope.member.$id}}, $scope, "tanks")
+
+    $scope.addTank = function () {
+        debugger;
+        $scope.tank.userId = $rootScope.member.$id
+        Models.Tank.create($scope.tank).then(function (tank) {
+                   $rootScope.member.tanks = $rootScope.member.tanks || [];
+                   $rootScope.member.tanks.push(tank.id);
+                   $rootScope.member.$save()
+            });
+        $scope.tank = ''
     }
+    
+    
 });
+
+
