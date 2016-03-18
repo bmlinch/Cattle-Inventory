ba07bcec9a739e1afbb641ca4a71a77d02884678
Accidental82 <Jason.sullenger@gmail.com>
2016-03-18 15:54:14 -0600

    adds update form

--- public/apps/components/herdOverview/herdOverview.js
+++ public/apps/components/herdOverview/herdOverview.js
@@ -1,20 +1,13 @@
 app.controller('HerdOverview', function($scope, $stateParams, $state, Models){
-    // Models.Cow.findAll();
-    // Models.Cow.bindAll({where:{herdId: $stateParams.herdId}}, $scope, 'cows')
-    
-    
     $scope.herdId = $stateParams.herdId;
     Models.Herd.find($stateParams.herdId)
     Models.Herd.bindOne($stateParams.herdId, $scope, "herd")
-    
-    
-    
+      
     $scope.addCow = function(){
         $state.go("addCow", {herdId: $stateParams.herdId})
     }
     
-
-   
-    
-    
+    $scope.updateCow = function(){
+        debugger;
+    }   
 });
