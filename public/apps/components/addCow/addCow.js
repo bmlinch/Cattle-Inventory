71f29488c6cf14acaab25a1e6b29309420117d5d
Accidental82 <Jason.sullenger@gmail.com>
2016-03-17 18:50:05 -0600

    stuff

--- public/apps/components/addCow/addCow.js
+++ public/apps/components/addCow/addCow.js
@@ -3,7 +3,8 @@ app.controller('AddCow', function($rootScope, $state, $scope, Models, $statePara
     Models.Herd.findAll()
     Models.Herd.bindOne($stateParams.herdId, $scope, "herd")
     
-    $scope.addCow = function () {
+    $scope.addCow = function (e) {
+        e.preventDefault();
         if(!$scope.herd.cows){
             $scope.herd.cows = [];
         }
@@ -11,6 +12,6 @@ app.controller('AddCow', function($rootScope, $state, $scope, Models, $statePara
         $scope.herd.cows.push($scope.cow);
         $scope.herd.DSSave();
         $scope.cow = '';
-        $state.go("herdOverview");
+        $state.go("herdOverview", {herdId: $state.params.herdId});
     }   
 });
